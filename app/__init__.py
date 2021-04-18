7d80802e87e779e96a6dcc4a80a81ca539684e3e
Kevin Shah <kevinshahonnet@gmail.com>
2021-04-18 11:04:44 +0530

    Added requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-import pymongo
+import pymongo, os
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = '892gf{r89094r2{98ihkb230JKBKjdfv' #random key
 app.debug = True
 login = LoginManager(app)
 login.login_view = 'logview'
-p = Path("./.env")
+p = Path("../.env")
 load_dotenv(dotenv_path = p)
 client = pymongo.MongoClient(os.getenv("MONGO"))
 db = client["CollegeBuddy"]
